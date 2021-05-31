0d2c8d46ce502ab639377233522a8d0cf8abf7a4
German Enik <germanenik@gmail.com>
2021-05-31 14:29:32 -0700

    temp prune nothing and track running stats

--- train.py
+++ train.py
@@ -109,7 +109,8 @@ def train_gmm(opt, train_loader, model, board):
                 print("interest layer num:", idx)
                 # Compute Weight Value attributions
                 attr = attribution.run(module)
-                k = int(len(attr) / 20) #5%
+                #k = int(len(attr) / 20) #5%
+                k = 0
                 pruning_indices = np.argpartition(attr, k)[:k]
 
                 cascading = layers_of_interest[idx+1:]
@@ -146,6 +147,7 @@ def _train_gmm(opt, train_loader, model, criterionL1, gicloss, optimizer, board,
             im_c = inputs['parse_cloth'].to(device)
             im_g = inputs['grid_image'].to(device)
             grid, theta = model(agnostic, cm)    # can be added c too for new training
+            #breakpoint()
             warped_cloth = F.grid_sample(c, grid, padding_mode='border')
             warped_mask = F.grid_sample(cm, grid, padding_mode='zeros')
             warped_grid = F.grid_sample(im_g, grid, padding_mode='zeros')
